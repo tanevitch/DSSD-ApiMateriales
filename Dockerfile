df66e5fddf9be73cd2f22c0023fdae4aa48754dd
alejopugli <alejopugli@gmail.com>
2022-10-20 20:14:35 -0300

    Deploy con waitress para hacer production ready

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,4 @@ FROM python:3.10.7-alpine3.16
 COPY ./ ./app
 WORKDIR /app
 RUN pip install -r requirements.txt 
-CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
-
-
-
+CMD ["waitress-serve", "--call", "app:create_app"]
--- config.py
+++ config.py
@@ -15,11 +15,11 @@ class Config(object):
 class ProductionConfig(Config):
     """Production configuration."""
 
-    DB_HOST = environ.get("DB_HOST", "localhost")
-    DB_USER = environ.get("DB_USER", "MY_DB_USER")
-    DB_PASS = environ.get("DB_PASS", "MY_DB_PASS")
-    DB_NAME = environ.get("DB_NAME", "MY_DB_NAME")
-    SQLALCHEMY_DATABASE_URI= (f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}")
+    DB_HOST = environ.get("DB_HOST", "")
+    DB_USER = environ.get("DB_USER", "")
+    DB_PASS = environ.get("DB_PASS", "")
+    DB_NAME = environ.get("DB_NAME", "api")
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///' + os.path.join(basedir, 'database.db')
 
 
 # class DevelopmentConfig(Config):
