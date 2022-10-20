ddbfda6c217c36bf22a78fc176d59444df430d41
alejopugli <alejopugli@gmail.com>
2022-10-20 20:35:02 -0300

    Deleted last "/"

--- app/controllers/materiales.py
+++ app/controllers/materiales.py
@@ -3,7 +3,7 @@ from app.models.material import Material
 materiales = Blueprint('materiales', __name__, url_prefix='/materiales')
 
 
-@materiales.route("/", methods=["GET"])
+@materiales.route("", methods=["GET"])
 def todos():
     resp = jsonify([material.toJSON() for material in Material.all()])
     resp.headers['Access-Control-Allow-Origin'] = '*'
--- app/controllers/proovedores.py
+++ app/controllers/proovedores.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 proovedores = Blueprint('proveedores', __name__, url_prefix='/proveedores')
 
 
-@proovedores.route("/", methods=["GET"])
+@proovedores.route("", methods=["GET"])
 @jwt_required()
 def protected():
     current_user = get_jwt_identity()
