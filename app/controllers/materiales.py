a5811fb0347c0096192963e46a71c775fe0394d7
Pablo <pablo_unlp@eyahoo.com.ar>
2022-10-20 17:53:36 -0300

    cors en materiales

--- app/controllers/materiales.py
+++ app/controllers/materiales.py
@@ -5,4 +5,7 @@ materiales = Blueprint('materiales', __name__, url_prefix='/materiales')
 
 @materiales.route("/", methods=["GET"])
 def todos():
-    return jsonify([material.toJSON() for material in Material.all()]), 200
+    resp = jsonify([material.toJSON() for material in Material.all()])
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    return resp, 200
