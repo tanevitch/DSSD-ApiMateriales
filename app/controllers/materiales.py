4d8941b372720be9be9622138fbf82270f4a2da8
tanevitch <lubatane@gmail.com>
2022-11-17 16:14:12 -0300

    feat: buscar por id

--- app/controllers/materiales.py
+++ app/controllers/materiales.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from app.models.material import Material
 from app.schemas.material import materiales_schema
+from app.schemas.material import material_schema
 
 materiales = Blueprint('materiales', __name__, url_prefix='/materiales')
 
@@ -8,4 +9,9 @@ materiales = Blueprint('materiales', __name__, url_prefix='/materiales')
 @materiales.route("", methods=["GET"])
 def todos():
     resp = materiales_schema.dump(Material.all())
+    return resp, 200
+
+@materiales.route("/<int:id>", methods=["GET"])
+def get(id):
+    resp = material_schema.dump(Material.buscarPorId(id))
     return resp, 200
--- app/controllers/sedes.py
+++ app/controllers/sedes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from app.models.sede import Sede
 from app.schemas.sede import sedes_schema
+from app.schemas.sede import sede_schema
+
 
 sedes = Blueprint('sedes', __name__, url_prefix='/sedes')
 
@@ -8,4 +10,9 @@ sedes = Blueprint('sedes', __name__, url_prefix='/sedes')
 @sedes.route("", methods=["GET"])
 def todos():
     resp = sedes_schema.dump(Sede.all())
+    return resp, 200
+
+@sedes.route("/<int:id>", methods=["GET"])
+def get(id):
+    resp = sede_schema.dump(Sede.buscarPorId(id))
     return resp, 200
